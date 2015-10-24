f555e55dfad92203f8e7bb3dc9fd102b2fcdc6dc
Attila Györffy <attila@attilagyorffy.com>
2015-10-24 20:23:38 +0200

    Fix database config in development
    The default role 'postgres' that Phoenix uses does not exist.

--- config/dev.exs
+++ config/dev.exs
@@ -35,8 +35,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :peep_blog_backend, PeepBlogBackend.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: "attila",
+  password: "",
   database: "peep_blog_backend_dev",
   hostname: "localhost",
   pool_size: 10
