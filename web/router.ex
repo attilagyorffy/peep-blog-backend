38401a276d6355b19a4bef17cc3cb6c1fb15411a
Attila Györffy <attila@attilagyorffy.com>
2015-10-24 20:42:22 +0200

    Replace browser pipeline in the router with api pipeline

--- web/router.ex
+++ web/router.ex
@@ -1,20 +1,12 @@
 defmodule PeepBlogBackend.Router do
   use PeepBlogBackend.Web, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
-  end
-
   pipeline :api do
     plug :accepts, ["json"]
   end
 
   scope "/", PeepBlogBackend do
-    pipe_through :browser # Use the default browser stack
+    pipe_through :api
 
     get "/", PageController, :index
   end
